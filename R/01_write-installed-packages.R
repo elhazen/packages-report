442f5d23666c31a806eee66517f3855e68ece395
elhazen <ehazen@gmail.com>
2018-02-01 11:44:08 -0800

    re-did Jenny's code for my packages

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,7 +1,34 @@
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
+#' ---
+#' output: github_document
+#' ---
+library(fs)
+library(tidyverse)
+library(stringr)
+library(igraph)
+library(ggraph)
+library(here)
 
+
+## how jenny might do this in a first exploration
+## purposely leaving a few things to change later!
+
+#' Which libraries does R search for packages?
+.libPaths()
+
+## let's confirm the second element is, in fact, the default library
+.Library
+
+path_real(.Library)
+
+#' Installed packages
+
+ipt <- installed.packages() %>%
+  as_tibble() %>% select("Package","LibPath","Version","Priority","Built")
+
+write_csv(ipt, here("data","installed-packages.csv"))
 ## keep the variables
 ##   * Package
 ##   * LibPath
